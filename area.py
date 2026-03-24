0e273a034985325699b597f06524eb63d92cf831
Abir-DW <abirwahab15@gmail.com>
2026-03-24 12:59:19 +0530

    Delete area.py

--- area.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def area_circle(radius):
-    return math.pi * radius * radius
-
-def area_rectangle(length, width):
-    return length * width
-def area_triangle(base, height):
-    return 1/2 * base * height

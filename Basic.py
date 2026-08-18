d74250f1dbc63e526bdae7bc1b52d6f98006b3a2
Abir-DW <abirwahab15@gmail.com>
2026-08-18 06:39:47 +0530

    Add files via upload

---
+++ Basic.py
@@ -0,0 +1,53 @@
+#ques 1
+print("Hello, World!")
+
+#ques 2
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+print("The sum of the code is",a+b)
+
+#ques 8
+num=int(input("Enter the number"))
+sq=(num*num)
+cube= (num*num*num)
+print("The square of the number is",sq,"and the cube is", cube)
+
+#Conditional statements 
+
+#ques 12
+year=int(input("Enter the year to be checked:"))
+if year%4==0:
+    print("It is leap year")
+else:
+    print("It is not the leap year")
+    
+#ques 13
+marks=int(input("Enter your marks:"))
+if marks>=91:
+    print("your grade is O")
+elif marks>=81:
+    print("Your grade is A+")
+elif marks>=71:
+    print("Your grade is A")
+elif marks>=61:
+    print("Your grade is B+")
+elif marks>=51:
+    print("Your grade is B")
+elif marks>=41:
+    print("Your grade is C")   
+elif marks>=31:
+    print("Your grade is D")
+elif marks>=21:
+    print("Your grade is E")
+else:
+    print("You are FAIL")
+          
+          
+#ques 14
+vwls=[a,e,i,o,u,]
+word=input("Enter the word:")
+if f[0] in word == vwls[0][1][2][3][4]:
+    print("It is a vowel")
+else:
+    print("It is a consonants")
+    

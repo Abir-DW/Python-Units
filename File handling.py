2a455fd586343a40ba81646c0fda59358e3ba694
Abir-DW <abirwahab15@gmail.com>
2026-08-18 06:41:15 +0530

    Add files via upload

---
+++ File handling.py
@@ -0,0 +1,6 @@
+# ques 52
+
+file = open("data.txt", "r")
+data = file.read()
+print(data)
+file.close()
---
+++ loops.py
@@ -0,0 +1,17 @@
+#ques 16
+
+for i in range(1,101):
+    print(i)
+    
+#ques 17
+
+num=int(input("Enter the number"))
+for i in range(1,11):
+    print(num*i)
+    
+#ques 18
+number=int(input("Enter the number"))
+fact=1
+for i in range(1,number+1):
+    fact= fact*i 
+print("The factorial is:",fact) 

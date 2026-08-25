8cbabc46f470449070cc0e08fbe89943b65eee85
Abir-DW <abirwahab15@gmail.com>
2026-08-25 09:12:00 +0530

    Update Conditional statements.py

--- SY/Conditional statements.py
+++ SY/Conditional statements.py
@@ -1,11 +1,9 @@
-#ques 12
 year=int(input("Enter the year to be checked:"))
 if year%4==0:
     print("It is leap year")
 else:
     print("It is not the leap year")
-    
-#ques 13
+
 marks=int(input("Enter your marks:"))
 if marks>=91:
     print("your grade is O")
@@ -25,8 +23,7 @@ elif marks>=21:
     print("Your grade is E")
 else:
     print("You are FAIL")
-          
-#ques 15
+
 a=int(input("Enter first number:"))
 b=int(input("Enter second number:"))
 c=int(input("Enter third number:"))
